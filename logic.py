d4c080dfeb1f994cd8a14d7fc6ac6d6b6a107621
htttpmnew-ship-it <htttpmnew@gmail.com>
2026-04-20 12:37:49 +0100

    update

--- logic.py
+++ logic.py
@@ -1,41 +1,105 @@
-from parsing import make_a_dictionary
+from parsing import make_a_dictionary,check_hub
 import re
-#parsing hub is the first because i will need here info
+
 #mission is make 2 array and display the place 
 #mission tomorrow is for make sure for parsing also add attribuite like the cost 
 #mission make sure u understand the dijikstra and do it 
 # the finale parsing for the part of connection
 
-class Cell:
-    def __init__(self,zone,visit):
-        pass
+class Grid:
+    def __init__(self,name, row, col, zone=0, color =0, max_drones = 0):
+        self.name = name 
+        self.row = row
+        self.col = col
+        self.zone = zone
+        self.color = color
+        self.max_drones = max_drones
+        self.place = 0
 
-def make_arr(position,end)-> list:
-    inside_lst = []
-    extra_lst = []
-    row, col = end
-    for row in range(row):
-        for col in range(col):
-            Ce
+
+def display(grid, start , end):
+    start_row, start_col = start
+    end_row, end_col = end
+    end_col = end_col+1
+    end_row = end_row+1
+    for x in range(start_row, end_row):
+        for y in range(start_col, end_col):
+            if grid[x][y].place == 1:
+                print(" + ",end="")
+            else:
+                print(" - ",end="")
+            
+        print()
     
+def ft_info(info, start,end):
+    start_row, start_col = start 
+    end_row ,end_col = end
+    row_list = []
+    grid = []
+    end_r = end_row +1
+    end_c = end_col+1
+    check = 0
+    for x in range(start_row, end_r):
+        row_list = []
+        for y in range(start_col, end_c):
+            step = Grid("anonymous",x,y)
+            row_list.append(step)
+        grid.append(row_list)
+  
+    grid[start_row][start_col].name = "start_hub"
+    grid[start_row][start_col].place = 1
+    grid[end_row][end_col].name = "start_end"
+    grid[end_row][end_col].place = 1
+    for key , value in info.items():
+        
+        row , col =value["position"]
+        row = int(row)
+        col = int(col)
+        name = value["name"]
+        try:
+            zone = value["zone"]
+        except:
+            zone = 0
+        try:
+            max_drone = value["max_drones"]
+            max_drone = int(max_drone)
+        except:
+            max_drone = 0
+        try:
+            color = value["color"]
+        except:
+            color = 0
+        grid[row][col].zone = zone
+        grid[row][col].place = 1
+        grid[row][col].name = name
+        grid[row][col].color = color
+        grid[row][col].max_drones = max_drone
+    
+    return grid
+    # for x in range(start_row, end_row):
+    #     for y in range(start_col, end_col):
+    #         print(grid[x][y].name)
+    #         print(grid[x][y].zone)
 
+
+# info()
 def main():
     dic = make_a_dictionary()
     position = []
     lst_position = []
     for key, value in dic.items():
-        if key.strip() == "hub":
-            key = key.strip()
-            # v = value.split()
+        if key.strip() == "start_hub":
             for x in value:
                 v = x.split()
-                position.append((key,(int(v[1]),int(v[2]))))
+                start = (int(v[1]),int(v[2]))
         if key.strip() == "end_hub":
             for x in value:
                 v = x.split()
                 end = (int(v[1]),int(v[2]))
-    print(end)
-    make_arr(position, end)
-
+    dic = make_a_dictionary()
+    info = check_hub(dic["hub"])
+    grid = ft_info(info, start,end)
+    display(grid,start, end)
 
 main()
+#ddd
--- parsing.py
+++ parsing.py
@@ -21,6 +21,25 @@ def make_a_dictionary():
                 dic[key] = [value]
     dic["prefix_v"] = [check_prefix]
     return dic
+def check_connection(connection, names):
+    try:
+        for x in connection:
+            x = x.strip().split("-")
+            if x[0] not in names :
+                raise ValueError("invalid name")
+            if x[1] not in names:
+                if "[" in x[1]:
+                    pairs = re.findall(r'(\w+)\s*=\s*(\w+)', x[1])
+
+                    print(pairs[0][0])
+
+                raise ValueError("invalid name")
+    except ValueError as e:
+        print("Error",e)    
+
+
+
+
 
 def check_start_end(start_hub):
     all_arg = []
@@ -69,15 +88,173 @@ def check_start_end(start_hub):
         if index != -1:
             for n in start_hub[index+1:]:
                 raise ValueError("Stop there")
-        
+
     except ValueError as e:
         print('\033[91m',"Error", e)
-        
+
+
+def check_hub(lines, dic):
+    the_same_name = []
+    dic_info = {}
+    for line in lines:
+        check = 0
+        c = 0
+        d = 0
+        zones = ["priority", "normal", "blocked","restricted"]
+        colors = ["green", "yellow","red", "blue","gray"]
+        try:
+         
+            if "[" in line:
+                if line.count("]") > 1 or line.count("[") > 1 or "]" not in line:
+                    raise ValueError("Please enter the form like that [   ] on hub") 
+                line1, line2 = line.split("[")
+                line = line1.split()
+                line.append("["+line2)
+                name = line[0]
+                if not isinstance(name, str):
+                    raise ValueError("Please enter the name as string")
+                if "-" in name:
+                    raise ValueError("Don't write dashes on the name is forbids :)")
+                star_t = line[1]
+                start = int(star_t)
+                if not isinstance(start, int):
+                    raise ValueError("Please enter the start as int")
+                en_d = line[2]
+                end = int(en_d)
+                if not isinstance(end, int):
+                    raise ValueError("Please enter the end as int")
+                # print(line)
+                # print(name)
+                if name in the_same_name:
+                    raise ValueError("don't use the same name")
+                the_same_name.append(name)
+                if name not in dic_info:
+                    dic_info[name] = {}
+                meta = str(line[3])
+                # print(len(metadata))
+                che_k = meta.split("=")
+                
+                chek = che_k[-1]
+                chek = chek.split()
+                for i in chek:
+                    if not i.isspace():
+                        if isinstance(i ,str) and i != "]":
+                            d = 1 
+                    if d == 1 and isinstance(i ,str) and (i != "]"):
+                        c += 1
+                if c > 1:
+                    raise ValueError("don't do more than 2 value")
+                
+                
+                # print(name)
+                dic_info[name]["name"] = name
+                dic_info[name]["position"] =(star_t, en_d)
+                if len(meta.split("=")) == 2:
+                    metadata = meta.split("=")
+                    value = metadata[1].strip()
+                    metadata = metadata[0].strip()
+                    if metadata != "zone" and  metadata !="color" and metadata !="max_drones":
+                        raise ValueError("Please enter one of this value zone or color or max_drones")
+                    if metadata == "zone":
+                        if value not in zones:
+                            raise ValueError("Please enter one of this zone priority, normal, blocked, restricted")
+                    if metadata == "color":
+                            if value not in colors:
+                                raise ValueError("Please enter one of this colors : green , yellow, red , blue, gray")
+                    if metadata == "max_drones":
+                        value = int(value)
+                        if value <= 0:
+                            raise ValueError("Please the value must be up to the 0")
+                    dic_info[name][metadata] = value
+
+
+                if len(meta.split("=")) == 3:
+                    metadata = meta
+                    metadata = metadata.strip().replace("[", "").replace("]", "")
+                    pairs = re.findall(r'(\w+)\s*=\s*(\w+)', metadata)
+                    for x in pairs:
+                        metadata = x[0].strip()
+                        value = x[1].strip()
+                        if metadata != "zone" and  metadata !="color" and metadata !="max_drones":
+                            raise ValueError("Please enter one of this value zone or color or max_drones")
+                        if metadata == "zone":
+                            if value not in zones:
+                                raise ValueError("Please enter one of this zone priority, normal, blocked, restricted")
+                        if metadata == "color":
+                            if value not in colors:
+                                raise ValueError("Please enter one of this colors : green , yellow, red , blue, gray")
+                        if metadata == "max_drones":
+                            mvalue = int(value)
+                            if mvalue <= 0:
+                                raise ValueError("Please the value must be up to the 0")
+                        dic_info[name][metadata] = value
+                    if len(pairs) !=2:
+                        raise ValueError("don't take the value empty")
+                if len(meta.split("=")) == 4:
+                    metadata = meta
+                    metadata = metadata.strip().replace("[", "").replace("]", "")
+                    pairs = re.findall(r'(\w+)\s*=\s*(\w+)', metadata)
+                    for x in pairs:
+                        metadata = x[0].strip()
+                        value = x[1].strip()
+                        if metadata != "zone" and  metadata !="color" and metadata !="max_drones":
+                            raise ValueError("Please enter one of this value zone or color or max_drones")
+                        if metadata == "zone":
+                            if value not in zones:
+                                raise ValueError("Please enter one of this zones : priority, normal, blocked, restricted")
+                        if metadata == "color":
+                            if value not in colors:
+                                raise ValueError("Please enter one of this colors : green , yellow, red , blue, gray")
+                        if metadata == "max_drones":
+                            mvalue = int(value)
+                            if mvalue <= 0:
+                                raise ValueError("Please the value must be up to the 0")
+                            
+                        dic_info[name][metadata] = value 
+                    if len(pairs) != 3:
+                            raise ValueError("don't take the value empty")
+                line = " ".join(line)
+                if line.count("]") > 1:
+                    raise ValueError("you need to enter the form of meta like that [...]")
+                for x in line:
+                    if x == "]":
+                        check = 1
+                    if check == 1 and x == "]" :
+                        continue
+                    if(x and check) and not x.isspace():
+                        raise ValueError("please don't write after ]")    
+            else:
+                name = line
+                if not isinstance(name, str):
+                    raise ValueError("Please enter the name as string")
+                if "-" in name:
+                    raise ValueError("Don't write dashes on the name is forbids :)")
+                start = line[1]
+                start = int(start)
+                if not isinstance(start, int):
+                    raise ValueError("Please enter the start as int")
+                end = line[2]
+                end = int(end)
+                if not isinstance(end, int):
+                    raise ValueError("Please enter the end as int")
+                #this i don't need this check
+
+            
+                    
+        except ValueError as e:
+            print("Error", e)
+            return False
+    the_same_name.extend(["hub","goal"])
+    connection = check_connection(dic["connection"],the_same_name)
+    if not connection:
+        return False
+    return dic_info
+    
+#zone=restricted color=red 
 def check_validation():
     dic = make_a_dictionary()
-    # for key, value in dic.items:
     if dic["prefix_v"][0] > 0:
-        print(dic["prefix_v"][0])
+        # print(dic["prefix_v"][0])
         print("Error")
     nb_drones = dic["nb_drones"][0]
 
@@ -99,8 +276,9 @@ def check_validation():
     check_start_end(start_hub)
     end_hub = dic["end_hub"][0].strip()
     check_start_end(end_hub)
-    check_hub = check_hub()
-    # print(arg[0])
+    info = check_hub(dic["hub"],dic)
+    if not info:
+        return
 
 check_validation()
 
@@ -135,82 +313,110 @@ check_validation()
 
 
 
-# this is for check all metadata
 
+# import re
+# def make_a_dictionary():
+#     dic = {}
+#     check_prefix = 0
+#     with open("configuration.txt", "r") as file:
+#         lines = file.readlines()
+#         for line in lines:
+#             line = line.strip()
+#             if not line or ":" not in line:
+#                 if ":" not in line and line:
+#                     check_prefix +=1
+#                 continue
+#             key , value= line.split(":", 1)
+#             key = key.lower()
+#             #prefix    
+#             if key != "start_hub" and key != "end_hub" and key != "hub" and key != "connection" and key != "nb_drones":
+#                 check_prefix += 1
+#             if key in dic:
+#                 dic[key].append(value)
+#             else:
+#                 dic[key] = [value]
+#     dic["prefix_v"] = [check_prefix]
+#     return dic
 
-
-        # if arg[3]:
-        #     all_arg.append(arg[3])
-        #     if arg[4]:
-        #         all_arg.append(arg[4])
-        #         # if arg[5]:
-        #         #     all_arg.append(arg[5])
-        # print(all_arg)
-        # if re.search("color",arg[3]):
-        #     pass
+# def check_start_end(start_hub):
+#     all_arg = []
+#     arg = start_hub.split()
+#     lst_color = ["green", "yellow", "red", "blue", "gray"]
+#     lst_zones = ["restricted","normal","priority","blocked"]
+#     try:
+#         name = arg[0].strip().replace('"','')
+#         if not name.isalpha():
+#             raise ValueError("please inter name as string :)")
+#         elif "-" in name:
+#             raise ValueError("Don't write dashes on the name is forbids :)")
+#         start = arg[1].strip().replace('"','')
+#         start = int(start)
+#         if start < 0:
+#             raise ValueError("Number must be positive")
+#         end = arg[2].strip().replace('"','')
+#         end = int(end)
+#         if start < 0:
+#             raise ValueError("Number must of be positive")
+#         if "[" not in start_hub and "]" not in start_hub or start_hub.count('[') > 1 or start_hub.count(']') > 1:
+#             raise ValueError("Please enter the form between [ ]")
+#         check = 0
+#         temp = ""
+#         for x in start_hub:
+#             if x =="[":
+#                 check = 1
+#             elif x == "]":
+#                 check = 0
+#             elif check:
+#                 temp +=x
             
-        #     all_arg = str(all_arg).replace('[','',1).replace(']','',1)
-        #     # last_index = len(all_arg) -1 
-        #     print(all_arg)
-        #     if all_arg[0] != "[" and "]" != all_arg[-1] or all_arg.count('[') > 1 or all_arg.count(']') > 1:
-        #         raise ValueError("Please enter the form between [ ]")
-        #     all_arg = str(all_arg).replace('[','',1).replace(']','',1)
-        #     arg = all_arg.split(",")
-
-        #     if "," in arg[3]:
-        #         arg = arg[3].split(",")
-        #         print(arg)
- 
-            # if len(arg) > 3:
-            #     raise ValueError("more than 3 please enter one of : zone, color or max_drones")
-            # for n in arg:
-            #     argument = n.split("=")
-            #     if len(argument) != 2:
-            #         raise ValueError("Please enter the form like that  metadata = value")
-            #     metadata = argument[0].lower()
-            #     if metadata == "color" or metadata == "zone":
-            #         value = argument[1].lower().replace("'",'').strip()
-            #     else:
-            #         value = argument[1].replace("'",'').strip()
-            #     metadata = metadata.replace("'",'').strip()
-            #     if metadata != "color" and metadata != "zone" and metadata != "max_drones":
-            #         raise ValueError("Please enter one of this color or zone or max_drones")
-            #     if metadata == "color" and value not in lst_color:
-            #         raise ValueError("Please enter one of this colors green or yellow, red, blue, gray")
-            #     if metadata == "zone" and value not in lst_zones:
-            #         raise ValueError ("Please enter one of this zones restricted,normal,priority,blocked")
-            #     if metadata == "max_drones":
-            #         try:
-            #             value = int(value)
-            #             if value < 0:
-            #                 print("Please enter a positive number")
-            #         except ValueError:
-            #             print("Error: Please enter a int value en max_drones")
-
-        #     else:
-        #         arg[3] = arg[0].split("=")
-        #         if len(arg[3]) != 2:
-        #             raise ValueError("Please enter the form like that  metadata = value")
-        #         metadata = arg[3][0].lower()
-        #         if metadata == "color" or metadata == "zone":
-        #             value = arg[3][1].lower()
-        #         else:
-        #             value = arg[3][1]
-        #         if metadata != "color" and metadata != "zone" and metadata != "max_drones":
-        #             raise ValueError("Please enter one of this color or zone or max_drones")
-        #         if metadata == "color" and value not in lst_color:
-        #             raise ValueError("Please enter one of this colors green or yellow, red, blue, gray")
-        #         if metadata == "zone" and value not in lst_zones:
-        #             raise ValueError ("Please enter one of this zones restricted,normal,priority,blocked")
-        #         if metadata == "max_drones":
-        #             try:
-        #                 value = int(value)
-        #                 if value < 0:
-        #                     print("Please enter a positive number")
-        #             except ValueError:
-        #                 print("Error: Please enter a int value en max_drones")
-
-        # if len(arg) > 4:
-        #     raise ValueError("The arg is more then 4")  
-
+#         temp = temp.split("=")
+#         if len(temp) != 2:
+#             raise ValueError("Please enter the form like that  metadata = value")
+#         if temp[0].lower().strip() == "color":
+#             value = temp[1].lower().strip()
+#             metadata = temp[0].lower().strip()
+#         else:
+#             raise ValueError("Please enter the color")
+#         if not isinstance(value, str) or not isinstance(metadata,str):
+#             raise ValueError("Enter the color and the value as string")
+#         if metadata == "color" and value not in lst_color:
+#             raise ValueError("Please enter one of this colors green or yellow, red, blue, gray")
+#         index = start_hub.find(']')
+#         if index != -1:
+#             for n in start_hub[index+1:]:
+#                 raise ValueError("Stop there")
         
+#     except ValueError as e:
+#         print('\033[91m',"Error", e)
+        
+# def check_validation():
+#     dic = make_a_dictionary()
+#     # for key, value in dic.items:
+#     if dic["prefix_v"][0] > 0:
+#         print(dic["prefix_v"][0])
+#         print("Error")
+#     nb_drones = dic["nb_drones"][0]
+
+#     try:
+#         nb_drones = nb_drones.strip().replace('"','')
+#         nb_drones = int(nb_drones)
+#         if nb_drones < 0:
+#             raise ValueError("Number must of nb_drones be positive")
+#     except ValueError as e:
+#         print("Error", e)
+#     if len(dic["start_hub"]) > 1:
+#         print("You can't write start_hub more than one")
+#     if len(dic["nb_drones"]) > 1:
+#         print("You can't write nb_drones more than one")
+#     if len(dic["end_hub"]) > 1:
+#         print("You can't write end_hub more than one")
+
+#     start_hub = dic["start_hub"][0].strip()
+#     check_start_end(start_hub)
+#     end_hub = dic["end_hub"][0].strip()
+#     check_start_end(end_hub)
+#     check_hub = check_hub()
+#     # print(arg[0])
+
+# check_validation()
+
